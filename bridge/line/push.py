ebf73b265cad85c4e36bbfe2a361fac3b231fd15
SuperSonic <supersonic@livemail.tw>
2021-02-28 02:21:11 +0800

    Fix bug

--- bridge/line/push.py
+++ bridge/line/push.py
@@ -6,7 +6,7 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
 from linebot import LineBotApi
-from linebot.models import TextSendMessage, Sender
+from linebot.models import TextMessage, Sender
 
 from .. import config
 
@@ -19,7 +19,7 @@ def text(sender: dict, receiver: str, content: str):
     sender = Sender(**sender)
     client.push_message(
         receiver,
-        TextSendMessage(
+        TextMessage(
             sender=sender,
             text=content
         )

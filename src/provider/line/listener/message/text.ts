5c58e8f5f256f10be46ea7ddc0c2d84d7aecb7d3
SuperSonic <supersonic@livemail.tw>
2022-06-28 00:17:26 +0800

    Update

--- src/provider/line/listener/message/text.ts
+++ src/provider/line/listener/message/text.ts
@@ -6,24 +6,50 @@ import {
 } from "@line/bot-sdk";
 
 import {
+    sendTextMessage
+} from "../../../matrix/sender";
+
+import {
     client,
 } from "../../index";
 
+const chatRoomId = process.env.MATRIX_CHAT_ROOM_ID || "";
+
+const commands: { [key: string]: any } = {
+    "getChatRoomId": (event: MessageEvent) => {
+        const sourceId: string = (() => {
+            switch (event.source.type) {
+                case "user":
+                    return event.source.userId;
+                case "group":
+                    return event.source.groupId;
+                case "room":
+                    return event.source.roomId;
+                default:
+                    return "";
+            }
+        })();
+        const replyMessage: TextMessage = {
+            type: "text",
+            text: sourceId,
+        };
+        return client.replyMessage(event.replyToken, replyMessage);
+    }
+};
+
 // Function handler to receive the text.
-export default async (
+export default (
     event: MessageEvent
-): Promise<MessageAPIResponseBase | undefined> => {
-    // Process all message related variables here.
-    const { replyToken } = event;
-    const message: TextEventMessage = <TextEventMessage>event.message;
+): Promise<MessageAPIResponseBase | undefined> | undefined => {
+    const message: TextEventMessage = event.message as TextEventMessage;
     const { text } = message;
 
-    // Create a new message.
-    const response: TextMessage = {
-        type: "text",
-        text,
-    };
+    if (text.startsWith("#") && text.substring(1).length > 0) {
+        const command = text.substring(1);
+        if (command in commands) {
+            return commands[command](event);
+        }
+    }
 
-    // Reply to the user.
-    return await client.replyMessage(replyToken, response);
+    sendTextMessage(text, chatRoomId);
 };
